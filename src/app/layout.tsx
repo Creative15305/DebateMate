f44452caacb613ec2dfaf4ee2456a7c1481bd077
Ayush Kumar Gaur <132849148+Ayush3941@users.noreply.github.com>
2025-06-29 00:01:12 +0530

    Fixed emascript error #ieeesoc (#75) #ieeesoc
    * Fixed emascript error
    
    * modified readme
    
    ---------
    
    Co-authored-by: = <=>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
+
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body>
